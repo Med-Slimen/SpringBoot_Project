86fe821269e78bc731756df198f7897ae1ae0513
Med-Slimen <mohamedamineslimene01@gmail.com>
2026-02-22 14:38:09 +0100

    first commit

---
+++ projects/src/main/java/com/slimene/projects/entities/Project.java
@@ -0,0 +1,11 @@
+package com.slimene.projects.entities;
+
+import java.util.Date;
+
+public class Project {
+	private Long idProject;
+	private String nomProject;
+	private String nomClient;
+	private Date dateDeb;
+	private Double montantProject;
+}
